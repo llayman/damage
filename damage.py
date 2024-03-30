3ddb5cdd2be6eabf2839dc1c1133c8433ff3afc2
Lucas Layman <laymanl@uncw.edu>
2024-03-30 09:42:54 -0400

    more refactoring

--- damage.py
+++ damage.py
@@ -1,57 +1,28 @@
 import random
+import re
+
+from typing import List
+
+DIE_PATTERN = re.compile(r"(\d)+d(\d+)([+-]\d+)?", re.IGNORECASE)
+
+
+def parse_dice(dmg: str) -> (int, int, int):
+    dmg = "".join(dmg.split())
+    parts = re.fullmatch(DIE_PATTERN, dmg)
+    if not parts:
+        raise ValueError
+
+    num_roll = int(parts[1])
+    die = int(parts[2])
+    modifier = 0 if parts.lastindex == 2 else int(parts[3])
+
+    return num_roll, die, modifier
 
 
 class Weapon:
 
-    def __init__(self, name, damage, extra_dmg='0'):
-        self.name = name
-        self.damage = damage
-        self.xtra_dmg = extra_dmg
-        self.flat = None
-        self.num_rolls, self.dice, self.modifier = self._split(damage)
-        self.xtra_num_rolls, self.xtra_dice, self.xtra_modifier = self._split(extra_dmg)
-
-    def _split(self, dmg):
-        if str(dmg) == '0':
-            return 0, 0, 0
-        dmg = "".join(dmg.split()).lower()
-        mod_str = dmg.split(r'+')
-        modifier = 0
-        num_roll = 0
-        die = 0
-
-        if len(mod_str) == 2:
-            modifier = int(mod_str[1])
-        elif len(mod_str) > 2:
-            raise ValueError("Too many +'s")
-
-        # Could be flat or <x>d<y>
-        dmg = mod_str[0]
-        die_str = dmg.split('d')
-        if len(die_str) == 1:
-            # flat damage
-            # XXX: Won't work if extra also has flat damage
-            self.flat = int(die_str[0])
-        elif len(die_str) != 2:
-            raise ValueError("Too many d's")
-        else:
-            num_roll = int(die_str[0])
-            die = int(die_str[1])
-
-        return num_roll, die, modifier
-
-    def damage_roll(self, crit=False):
-        if self.flat:
-            return self.flat
-
-        damage = self._roll(self.num_rolls, self.dice, crit) + self.modifier
-
-        if self.xtra_dmg:
-            damage += self._roll(self.xtra_num_rolls, self.xtra_dice, crit) + self.xtra_modifier
-
-        return damage
-
-    def _roll(self, num, die, crit):
+    @staticmethod
+    def roll(num: int, die: int, crit: bool) -> int:
         damage = 0
         for _ in range(num):
             damage += random.randint(1, die)
@@ -59,108 +30,203 @@ class Weapon:
                 damage += random.randint(1, die)
         return damage
 
+    def damage_roll(self, crit: bool = False) -> int:
+        raise NotImplementedError
+
+    def atk_mod(self) -> int:
+        raise NotImplementedError
+
+    def __str__(self) -> str:
+        raise NotImplementedError
+
+
+class FlatDamageWeapon(Weapon):
+
+    def __init__(self, name: str, dmg: int):
+        self.name = name
+        self.dmg = dmg
+
+    def damage_roll(self, crit=False):
+        return self.dmg if not crit else self.dmg * 2
+
+    def atk_mod(self) -> int:
+        return 0
+
     def __repr__(self):
-        return f"Weapon({self.name}, {self.damage}, {self.xtra_dmg})"
+        return f"FlatDamageWeapon({self.name}, {self.dmg}"
 
     def __str__(self):
-        _result = self.name + " ("
-        if self.flat:
-            _result += str(self.flat)
-            return _result
-        _result += f"{self.num_rolls}d{self.dice}"
-        if self.modifier != 0:
-            _result += f'+{self.modifier}'
-        return _result + ')'
+        return f"{self.name} ({self.dmg} flat)"
 
 
-def calc_damage(attack: int, dice: int, ability: int = 0, other_dmg=0, crit=20, num_dice: int = 1):
-    damage = 0
-    for _ in range(num_dice):
-        damage += random.randint(1, dice)
-        if attack >= crit:
-            damage += random.randint(1, dice)
-    return damage + other_dmg + ability
+class SimpleWeapon(Weapon):
+    @staticmethod
+    def from_dice(name, damage: str, extra_dmg: str = None) -> Weapon:
+        rolls, die, mod = parse_dice(damage)
+        extra = parse_dice(extra_dmg) if extra_dmg else (0, 0, 0)
+        return SimpleWeapon(name, rolls, die, mod, mod, *extra)
+
+    @staticmethod
+    def roll(num, die, crit):
+        damage = 0
+        for _ in range(num):
+            damage += random.randint(1, die)
+            if crit:
+                damage += random.randint(1, die)
+        return damage
+
+    def __init__(self, name, num_rolls, die, atk_modifier=0, dmg_modifier=0,
+                 xtra_num_rolls=0, xtra_die=0, xtra_dmg_modifier=0):
+        self.name = name
+        self.num_rolls = num_rolls
+        self.die = die
+        self.atk_modifier = atk_modifier
+        self.dmg_modifier = dmg_modifier
+        self.xtra_num_rolls = xtra_num_rolls
+        self.xtra_die = xtra_die
+        self.xtra_dmg_modifier = xtra_dmg_modifier
+
+    def damage_roll(self, crit=False):
+        damage = Weapon.roll(self.num_rolls, self.die, crit) + self.dmg_modifier
+
+        if self.xtra_die:
+            damage += Weapon.roll(self.xtra_num_rolls, self.xtra_die, crit) + self.xtra_dmg_modifier
+
+        return damage
+
+    def atk_mod(self) -> int:
+        return self.atk_modifier
+
+    def __str__(self):
+        mod = ""
+
+        if self.atk_modifier == self.dmg_modifier and self.atk_modifier != 0:
+            mod = f'+{self.atk_modifier}'
+        elif self.atk_modifier != self.dmg_modifier:
+            mod = f', atk: {self.atk_modifier}, dmg: {self.dmg_modifier}'
+
+        xtra = ""
+        if self.xtra_die:
+            xtra_mod = ""
+            if self.xtra_dmg_modifier != 0:
+                xtra_mod = f'+{self.xtra_dmg_modifier}'
+            xtra = f" + ({self.xtra_num_rolls}d{self.xtra_die}{xtra_mod})"
+
+        return f"{self.name} ({self.num_rolls}d{self.die}{mod}){xtra}"
 
 
-def solas_weapon(sims: int, weapon: Weapon, num_attacks=1):
+def simulate_ranik(sims: int, weapons: List[Weapon], curse_percentage=0.0, num_attacks=1):
+    ability = 3
+    proficiency = 2
+    print(f"Raniks curse uptime: {curse_percentage:.0%}")
+
+    for weapon in weapons:
+        _attack_total = 0
+        _damage_total = 0
+        _num_crits = 0
+        for _ in range(sims):
+            for __ in range(num_attacks):
+                crit = False
+                attack = random.randint(1, 20)
+                _attack_total += attack + ability + proficiency + weapon.atk_mod()
+
+                if random.random() < curse_percentage:
+                    if attack >= 19:
+                        crit = True
+                        _num_crits += 1
+                    _damage_total += weapon.damage_roll(
+                        crit) + ability + proficiency  # pact weapon + cursed target add prof to dmg
+                else:
+                    if attack >= 20:
+                        crit = True
+                        _num_crits += 1
+                    _damage_total += weapon.damage_roll(crit) + ability  # pact weapon
+
+        print_results(f"Ranik's {weapon}", _attack_total / (sims * num_attacks), _damage_total / sims,
+                      _num_crits / (sims * num_attacks))
+
+
+def simulate_solas(sims: int, weapons: List[Weapon], num_attacks: int = 1):
     ability = 2
     proficiency = 2
     other_dmg = 2
 
-    _attack_total = 0
-    _damage_total = 0
-    _num_crits = 0
+    for weapon in weapons:
+        _attack_total = 0
+        _damage_total = 0
+        _num_crits = 0
 
-    for _ in range(sims):
-        for __ in range(num_attacks):
-            crit = False
-            attack = random.randint(1, 20)
-            _attack_total += attack + ability + proficiency
+        for _ in range(sims):
+            for __ in range(num_attacks):
+                crit = False
+                attack = random.randint(1, 20)
+                _attack_total += attack + ability + proficiency + weapon.atk_mod()
 
-            if attack >= 20:
-                crit = True
-                _num_crits += 1
-            _damage_total += weapon.damage_roll(crit) + ability + other_dmg
-
-    return _attack_total / (sims * num_attacks), _damage_total / sims, _num_crits * 100 / (sims * num_attacks)
-
-
-def ranik_weapon(sims: int, weapon: Weapon, curse_percentage, num_attacks=1):
-    ability = 3
-    proficiency = 2
-    other_dmg = 0
-
-    _attack_total = 0
-    _damage_total = 0
-    _num_crits = 0
-    for _ in range(sims):
-        for __ in range(num_attacks):
-            crit = False
-            attack = random.randint(1, 20)
-            _attack_total += attack + ability + proficiency
-
-            if random.random() < curse_percentage:
-                if attack >= 19:
-                    crit = True
-                    _num_crits += 1
-                _damage_total += weapon.damage_roll(crit) + ability + proficiency  # cursed target add prof to dmg
-            else:
                 if attack >= 20:
                     crit = True
                     _num_crits += 1
-                _damage_total += weapon.damage_roll(crit) + ability  # assumes pact weapon
+                _damage_total += weapon.damage_roll(crit) + ability + other_dmg
 
-    return _attack_total / (sims * num_attacks), _damage_total / sims, _num_crits * 100 / (sims * num_attacks)
+        print_results(f"Solas' {weapon}", _attack_total / (sims * num_attacks), _damage_total / sims,
+                      _num_crits / (sims * num_attacks))
+
+
+def simulate_daruth(sims: int, weapons: List[Weapon], num_attacks=1):
+    ability = 4
+    proficiency = 2
+
+    for weapon in weapons:
+        _attack_total = 0
+        _damage_total = 0
+        _num_crits = 0
+        for _ in range(sims):
+            for __ in range(num_attacks):
+                crit = False
+                attack = random.randint(1, 20)
+                _attack_total += attack + ability + proficiency + weapon.atk_mod()
+
+                if attack >= 20:
+                    crit = True
+                    _num_crits += 1
+                _damage_total += weapon.damage_roll(crit) + ability
+
+        print_results(f"Daruth's {weapon}", _attack_total / (sims * num_attacks), _damage_total / sims,
+                      _num_crits / (sims * num_attacks))
+
+
+HEADER = "{0:45}{1:>10}{2:>10}{3:>10}".format(f"Name", "Attack", "Damage", "Crit %")
+TEMPLATE = "{0:45}{1:>10.1f}{2:>10.1f}{3:>10.1%}"
 
 
 def print_results(name, attacks, damage, crits):
-    print(f"{name} attack: {attacks:.1f}, damage: {damage:.1f}, crits: {crits:.1f}%")
+    print(TEMPLATE.format(name, attacks, damage, crits))
 
 
 if __name__ == "__main__":
     SIMS = 100_000
+    print(f"{SIMS} sims")
+    print(HEADER)
 
-    curse = .4
+    battleaxe = SimpleWeapon("battleaxe", num_rolls=1, die=8, dmg_modifier=1)
+    battleaxe_2h = SimpleWeapon("battleaxe (2h)", num_rolls=1, die=10, dmg_modifier=1)
+    flametongue = SimpleWeapon.from_dice("flametongue(?) scimitar", "1d6", "2d6")
+    longsword = SimpleWeapon.from_dice("longsword", "1d8")
+    monk_hand = SimpleWeapon.from_dice("monk hands", "1d6")
 
-    w = battleaxe = Weapon("battleaxe", "1d8+1")
-    print_results(f"Ranik's {w}", *ranik_weapon(SIMS, battleaxe, curse))
+    raniks_weapons = [
+        battleaxe,
+        battleaxe_2h,
+        flametongue
+    ]
+    simulate_ranik(SIMS, raniks_weapons, curse_percentage=0.4)
 
-    w = battleaxe_2h = Weapon("battleaxe (2H)", "1d10+1")
-    print_results(f"Ranik's {w}", *ranik_weapon(SIMS, battleaxe_2h, curse))
+    solas_weapons = [
+        longsword,
+        flametongue
+    ]
+    simulate_solas(SIMS, solas_weapons)
 
-    w = flametongue = Weapon("flametongue scimitar?", "1d6", "2d6")
-    print_results(f"Ranik's {w}", *ranik_weapon(SIMS, flametongue, curse))
-
-    #
-    # ranik_flametongue_result = ranik_flametongue(SIMS, 3, curse_percentage=.4, num_attacks=2)
-    # print_results("Ranik's flametongue attack (multiattack)", *ranik_flametongue_result)
-    #
-    # flametongue = Weapon("Flametongue", "1d6", "2d6")
-    # print(flametongue)
-
-
-    longsword = Weapon("Longsword", "1d8")
-    result = solas_weapon(SIMS, longsword, 2)
-    print_results("Solas' longsword attack (weapon)", *result)
-
-
+    daruths_weapons = [
+        monk_hand
+    ]
+    simulate_daruth(SIMS, daruths_weapons)
